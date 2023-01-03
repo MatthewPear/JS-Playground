410825659e548bb83af3b93f84e03a949b8cf3b1
MatthewPear <matthewpear@hotmail.com>
2023-01-03 17:44:22 +0100

    mk4

--- main.js
+++ main.js
@@ -12,10 +12,21 @@ document.getElementById('button3').append(button);
 
 const myNewButton = document.createElement('button');
 
-document.body.appendChild(myNewButton)
-
 myNewButton.textContent = "Clik me 4";
 myNewButton.addEventListener('click', () => myFunction('4'));
 
+document.body.appendChild(myNewButton)
+
+function megusta(event) {
+    console.log(event);
+    
+}
+
+const myNewerButton = document.createElement('button');
+
+myNewButton.textContent = "Clik me 5";
+myNewButton.addEventListener('click', (evt) => megusta('evt'));
+
+document.body.appendChild(myNewerButton)
 
 
